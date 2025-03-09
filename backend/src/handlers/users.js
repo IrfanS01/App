bb10c3ae0afbdd45b41f056cda36c92d0dc1c003
IrfanS01 <isarac89@gmail.com>
2025-03-10 00:00:44 +0100

    getusersklart

--- backend/src/handlers/users.js
+++ backend/src/handlers/users.js
@@ -1,6 +1,16 @@
 const cognito = require("../config/cognito");
 const response = require("../utils/response");
 
+
+console.log("COGNITO_USER_POOL_ID TYPE:", typeof process.env.COGNITO_USER_POOL_ID);
+console.log("COGNITO_USER_POOL_ID VALUE:", JSON.stringify(process.env.COGNITO_USER_POOL_ID, null, 2));
+console.log("COGNITO_USER_POOL_ID TYPE:", typeof process.env.COGNITO_USER_POOL_ID);
+console.log("COGNITO_USER_POOL_ID VALUE (JSON):", JSON.stringify(process.env.COGNITO_USER_POOL_ID));
+console.log("COGNITO_USER_POOL_ID RAW:", process.env.COGNITO_USER_POOL_ID);
+
+
+
+
 module.exports.getUsers = async () => {
     const params = {
         UserPoolId: process.env.COGNITO_USER_POOL_ID,
