bc311be7d3ced9e5950823beed3d3d49fac88d70
IrfanS01 <isarac89@gmail.com>
2025-04-06 12:13:47 +0200

    literefresh

--- aliases.sh
+++
@@ -1,5 +0,0 @@
-# aliases.sh
-alias envcheck="cd /Users/irfansarac/Desktop/App01 && npm run validate-env"
-alias envexample="cd /Users/irfansarac/Desktop/App01 && npm run generate-env-example"
-alias deploy="cd /Users/irfansarac/Desktop/App01/backend && npx serverless deploy"
-alias dev="cd ~/path/to/App01 && npm run dev"
